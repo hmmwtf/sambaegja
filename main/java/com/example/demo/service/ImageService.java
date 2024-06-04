3a5dfdad494d49a003481dced86fe2c38dae61ba
sanguk-park <tkddnrtmxk@naver.com>
2024-06-04 15:47:39 +0900

    [HOTFIX] urgent patch [ADD] Main_Page

---
+++ main/java/com/example/demo/service/ImageService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.vo.ImageVO;
+
+public interface ImageService {
+	List<ImageVO> select();
+}
--- main/resources/static/js/index.js
+++ main/resources/static/js/index.js
@@ -2,7 +2,7 @@ var date = new Date();
 var hour = date.getHours();
 var minute = date.getMinutes();
 var second = date.getSeconds();
-if (hour == 10 && minute == 15) { // 24시 정각일때
+if (hour == 16 && minute == 22) { // 24시 정각일때
 	axios.delete('/sub_boardDelete')
 		.then(function(response) {
 			// handle success
@@ -42,7 +42,9 @@ $(function() {
 		if($('#login_id').val().length==0){
 			alert("로그인후 이용해 주세요.");
 			location.href='/login';
+			return false;
 		}
+		location.href="/prevBoard";
 	});
 	$("#add").click(function() { // 글쓰기
 		if ($("#login_id").val().length == 0) {
@@ -62,50 +64,62 @@ $(function() {
 	});
 	getSubBoard();
 	axios.get('/selectMainBoard')
-			.then(function(response) {
-				// handle success
-			console.log(response.data[0]);
+		.then(function(response) {
+			// handle success
+			console.log(response.data);
 			let data = response.data;
-			let boardTitle = response.data[0];
-			dateString=boardTitle.title;
-			dateFormat = new Date(dateString);
-			console.log("날짜 : ",dateFormat);
-			date=$("<span>"+dateFormat.getFullYear()+"년 "+(Number)(dateFormat.getMonth()+1)+"월 "+dateFormat.getDate()+"일</span>");
-			$(date).appendTo($("#boardDate"));
-			$("#main_board").html(" ");
-			for (d of data) {
-				div = $("<div style='width:800px;margin-left:30%;' onclick='boardClick(" + d.idx + ");'></div>");
-				textarea = $("<textarea id='board_content" + d.idx + "' cols='100' rows='10' style='cursor:pointer;'>" + d.content + "</textarea>");
-				$(textarea).appendTo(div);
-				//$("#main_board").html(div);
-				$(div).appendTo($("#main_board"))
+			console.log("data : ",data.length);
+			if(data.length>0){
+				let boardTitle = response.data[0];
+				dateString=boardTitle.title;
+				dateFormat = new Date(dateString);
+				console.log(dateFormat.getFullYear());
+				console.log("날짜 : ",dateFormat);
+				date=$("<span>"+dateFormat.getFullYear()+"년 "+(Number)(dateFormat.getMonth()+1)+"월 "+dateFormat.getDate()+"일</span>");
+				$(date).appendTo($("#boardDate"));
+				$("#main_board").html(" ");
+				for (d of data) {
+					div = $("<div style='width:800px;margin-left:30%;' onclick='boardClick(" + d.idx + ");'></div>");
+					textarea = $("<textarea id='board_content" + d.idx + "' cols='100' rows='10' style='cursor:pointer;'>" + d.content + "</textarea>");
+					$(textarea).appendTo(div);
+					//$("#main_board").html(div);
+					$(div).appendTo($("#main_board"))
+				}
 			}
-			})
-			.catch(function(error) {
-				// handle error
-				console.log(error);
-				console.log("?");
-			})
-			.then(function() {
-				// always executed
-			});
+		})
+		.catch(function(error) {
+			// handle error
+			console.log(error);
+			console.log("?");
+		})
+		.then(function() {
+			// always executed
+		});
 	$("#select").change(function() {//추천순, 날짜순
 		axios.get('/selectBoard?option=' + $("#select").val())
 			.then(function(response) {
 				// handle success
-				console.log(response.data);
 				let data = response.data;
 				$("#result").html("");
 				for (d of data) {
-					div = $("<div style='width:800px;margin-left:35%;'></div>");
-					textarea = $("<textarea id='sub_content" + d.idx + "' cols='70' rows='10'>" + d.content + "</textarea>");
+					// div = $("<div style='width:800px;margin-left:35%;'></div>");
+					// textarea = $("<textarea id='sub_content" + d.idx + "' cols='70' rows='10'>" + d.content + "</textarea>");
+					// span = $("<span id='likey" + d.idx + "'>" + d.likey + "</span>");
+					// button = $("<button onclick='del(" + d.idx + ");'  class='btn btn-outline-secondary btn-sm' >삭제</button>");
+					// img = $("<img style='margin-bottom:70px;cursor:pointer;width:50px;height:50px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/img/like1.png' alt='0'>");
+					// $(textarea).appendTo(div);
+					// $(span).appendTo(div);
+					// $(button).appendTo(div);
+					// $(img).appendTo(div);
+					div = $("<div class = 'blackShadow mention'></div>");
+					div2 = $("<div></div>")
+					p = $("<p id='sub_content" + d.idx + "' cols='70' rows='10'>" + d.content + "</p>");//이거 왜 textarea일 때는 되고 p일때는 안됨?
 					span = $("<span id='likey" + d.idx + "'>" + d.likey + "</span>");
-					button = $("<button onclick='del(" + d.idx + ");'  class='btn btn-outline-secondary btn-sm' >삭제</button>");
-					img = $("<img style='margin-bottom:70px;cursor:pointer;width:50px;height:50px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/images/like1.png' alt='0'>");
-					$(textarea).appendTo(div);
-					$(span).appendTo(div);
-					$(button).appendTo(div);
-					$(img).appendTo(div);
+					img = $("<img style='cursor:pointer;width:25px;height:25px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/img/like1.png' alt='0'>");
+					$(p).appendTo(div);
+					$(span).appendTo(div2);
+					$(img).appendTo(div2);
+					$(div2).appendTo(div);
 					$(div).appendTo($("#result"));
 				}
 			})
@@ -122,19 +136,28 @@ function getSubBoard() {
 	axios.get('/selectBoard?option=' + $("#select ").val())
 		.then(function(response) {
 			// handle success
-			console.log(response.data);
+			console.log("qwer : ",response.data);
 			let data = response.data;
 			$("#result").html("");
 			for (d of data) {
-				div = $("<div style='width:800px;margin-left:35%;'></div>");
-				textarea = $("<textarea id='sub_content" + d.idx + "' cols='70' rows='10'>" + d.content + "</textarea>");
-				button = $("<button onclick='del(" + d.idx + ");' class='btn btn-outline-secondary btn-sm'>삭제</button>");
+				// div = $("<div style='width:800px;margin-left:35%;'></div>");
+				// textarea = $("<textarea id='sub_content" + d.idx + "' cols='70' rows='10'>" + d.content + "</textarea>");
+				// button = $("<button onclick='del(" + d.idx + ");' class='btn btn-outline-secondary btn-sm'>삭제</button>");
+				// span = $("<span id='likey" + d.idx + "'>" + d.likey + "</span>");
+				// img = $("<img style='margin-bottom:70px;cursor:pointer;width:50px;height:50px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/img/like1.png' alt='0'>");
+				// $(textarea).appendTo(div);
+				// $(span).appendTo(div);
+				// $(button).appendTo(div);
+				// $(img).appendTo(div);
+				div = $("<div class = 'blackShadow mention'></div>");
+				div2 = $("<div></div>")
+				p = $("<p id='sub_content" + d.idx + "' cols='70' rows='10' style='font-size: 30px'>" + d.content + "</p>");//이거 왜 textarea일 때는 되고 p일때는 안됨?
 				span = $("<span id='likey" + d.idx + "'>" + d.likey + "</span>");
-				img = $("<img style='margin-bottom:70px;cursor:pointer;width:50px;height:50px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/images/like1.png' alt='0'>");
-				$(textarea).appendTo(div);
-				$(span).appendTo(div);
-				$(button).appendTo(div);
-				$(img).appendTo(div);
+				img = $("<img style='cursor:pointer;width:25px;height:25px;' class='check" + d.idx + "' onclick='likey(" + d.idx + ")' src='/img/like1.png' alt='0'>");
+				$(p).appendTo(div);
+				$(span).appendTo(div2);
+				$(img).appendTo(div2);
+				$(div2).appendTo(div);
 				$(div).appendTo($("#result"));
 			}
 		})
@@ -170,11 +193,11 @@ async function likey(idx) { // 좋아요 누를시
 		return false;
 	}
 	if ($(".check" + idx).attr('alt') == 0) {
-		$(".check" + idx).attr("src", "/images/like2.png");
+		$(".check" + idx).attr("src", "/img/like2.png");
 		$(".check" + idx).attr("alt", 1);
 		axios.post('/likePlus', {
-			content: $("#sub_content" + idx).val(),
-			idx: 1
+			'idx2': idx,
+			'idx': 1
 		})
 			.then(function(response) {
 				console.log(response);
@@ -184,11 +207,11 @@ async function likey(idx) { // 좋아요 누를시
 			});
 	}
 	else {
-		$(".check" + idx).attr("src", "/images/like1.png");
+		$(".check" + idx).attr("src", "/img/like1.png");
 		$(".check" + idx).attr("alt", 0);
 		axios.post('/likePlus', {
-			content: $("#sub_content" + idx).val(),
-			idx: 0
+			'idx2': idx,
+			'idx': 0
 		})
 			.then(function(response) {
 				console.log(response);
@@ -214,3 +237,22 @@ async function likey(idx) { // 좋아요 누를시
 		});
 
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+	const changeButton = document.getElementById('change_div');
+	const textareaContainer = document.getElementById('textarea-container');
+	const commentsContainer = document.getElementById('comments-container');
+	let toggleState = 0;
+
+	changeButton.addEventListener('click', function () {
+		if (toggleState === 0) {
+			textareaContainer.classList.remove('hidden-container');
+			commentsContainer.classList.add('hidden-container');
+			toggleState = 1;
+		} else {
+			textareaContainer.classList.add('hidden-container');
+			commentsContainer.classList.remove('hidden-container');
+			toggleState = 0;
+		}
+	});
+});
